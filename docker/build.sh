fd3bec29dbae1edb56d6be3832573f6859b09acd
Manoj Sharma <manoj.sharma@loblaw.ca>
2019-05-10 18:15:15 -0400

    update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,1 +1,1 @@
-docker build . -f docker/Dockerfile -t hello-world
+docker build . -f docker/Dockerfile -t gcr.io/$GOOGLE_CLOUD_PROJECT && docker tag gcr.io/$GOOGLE_CLOUD_PROJECT gcr.io/$GOOGLE_CLOUD_PROJECT/hello-world && docker push gcr.io/$GOOGLE_CLOUD_PROJECT/hello-world
