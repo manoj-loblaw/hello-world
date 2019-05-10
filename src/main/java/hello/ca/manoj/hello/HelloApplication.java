0b6e691ee0f9e5f83981f30fd27864f971104c85
Manoj Sharma <manoj.sharma@loblaw.ca>
2019-05-10 12:55:29 -0400

    Created spring boot hello world

---
+++ src/main/java/hello/ca/manoj/hello/HelloApplication.java
@@ -0,0 +1,12 @@
+package hello.ca.manoj.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/hello/ca/manoj/hello/controller/HelloController.java
@@ -0,0 +1,14 @@
+package hello.ca.manoj.hello.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings!";
+    }
+
+}
