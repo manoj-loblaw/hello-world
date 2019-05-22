38e5ef377526de15464a5fa7f7d60bc8e614c5d8
Appan Chhibber <appanchhibber@gmail.com>
2019-05-22 15:49:55 -0400

    * JPA dependency added for Springboot ** Crud operation added *** Application properties added

--- src/main/java/hello/ca/manoj/hello/controller/HelloController.java
+++ src/main/java/hello/ca/manoj/hello/controller/HelloController.java
@@ -1,14 +1,36 @@
 package hello.ca.manoj.hello.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import hello.ca.manoj.hello.domain.RoleRepository;
+import hello.ca.manoj.hello.domain.Roles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Role;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
 
 @RestController
 public class HelloController {
 
+    @Resource
+    RoleRepository roleRepository;
+
+
+
     @RequestMapping("/")
     public String index() {
-        return "Greetings!";
+        return "Greetings!"+"\n"+roleRepository.findAll();
+    }
+
+
+    @PostMapping("/add/role")
+    public String createRole(@Valid @RequestBody Roles role) {
+        try {
+            roleRepository.save(role);
+            return "Save was successful";
+        } catch (Exception e) {
+            return "Error saving the data";
+        }
     }
 
 }
---
+++ src/main/java/hello/ca/manoj/hello/domain/RoleRepository.java
@@ -0,0 +1,6 @@
+package hello.ca.manoj.hello.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Roles,String> {
+}
