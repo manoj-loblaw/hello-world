51b06dcba96167703576b182c2f12164945f2d49
Appan Chhibber <appanchhibber@gmail.com>
2019-05-27 16:27:53 -0400

    minor code refactoring done to show the role saved instead of Role Object

--- src/main/java/hello/ca/manoj/hello/domain/RoleRepository.java
+++ src/main/java/hello/ca/manoj/hello/domain/RoleRepository.java
@@ -1,6 +1,8 @@
 package hello.ca.manoj.hello.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Roles,String> {
 }
